f95af29f015fb5bcb3ab374d85fa3760e625e293
Nicole Cayambas <ncayambas@melistechnology.com>
2025-04-12 12:41:55 +0800

    setup

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <div className='app-desktop'>
+    </div>
+    <div className='app-taskbar'>
+
+    </div>
+    </>
+  )
+}
+
+export default App
